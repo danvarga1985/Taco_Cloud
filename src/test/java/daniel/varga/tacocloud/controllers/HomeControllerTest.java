495b2e76c7a9c23f7d5c71c90172bc2384b5ad2e
danvarga1985 <danvarga1985@gmail.com>
2020-09-28 21:01:29 +0200

    Adding HomeController + Test + Template

---
+++ src/test/java/daniel/varga/tacocloud/controllers/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package daniel.varga.tacocloud.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+}
